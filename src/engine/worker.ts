ec2a3babe6aa4f7ae2c5a8dc3a64ea6e77de0a71
nickelliottpimm <nickelliott@gmail.com>
2025-11-07 07:11:51 +1100

    Recent fixes: error

--- src/engine/worker.ts
+++ src/engine/worker.ts
@@ -1,7 +1,7 @@
 // src/engine/worker.ts
 /// <reference lib="webworker" />
 import type { Board, Player } from "../game/rules";
-import { chooseGreedy } from "./greedy";
+import { enumerateMoves } from "./greedy";
 import type { AIMove } from "./aiTypes";
 
 type RequestMsg = { type: "request"; board: Board; turn: Player };
@@ -10,7 +10,7 @@ type ReplyMsg   = { type: "reply"; move: AIMove | null };
 self.onmessage = (e: MessageEvent<RequestMsg>) => {
   const { type, board, turn } = e.data;
   if (type !== "request") return;
-  const best = chooseGreedy(board, turn);
+  const best = enumerateMoves(board, turn);
   const reply: ReplyMsg = { type: "reply", move: best };
   (self as any).postMessage(reply);
 };
